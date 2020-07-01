d31a2b17154b110dd2e97252dc83b60381925f92
Mikko Niemikorpi <mikefulli@gmail.com>
2020-07-01 20:00:33 +0300

    Added files currently used in production

---
+++ config.example.php
@@ -0,0 +1,7 @@
+<?php
+
+$settings = array(
+    "api_key" => "",
+    "site_name" => "Vaasa Hacklab Ovi Manager",
+    "log_file" => "/full/path/to/thelog.log"
+);
---
+++ templates/message.php
@@ -0,0 +1,9 @@
+<?php include 'header.php'; ?>
+		
+		<h1><?php echo $title; ?></h1>
+		<p>
+			<?php echo $message; ?>
+		</p>
+
+<?php include 'footer.php'; ?>
+						
