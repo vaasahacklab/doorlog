f8959c2c8fb30fd64fc3b6f0842d58387fe0984f
Mikko Niemikorpi <mikefulli@gmail.com>
2020-07-01 20:18:22 +0300

    Cleaned up code

--- index.php
+++ index.php
@@ -27,74 +27,86 @@ function logiin($phoneNumber, $message = '', $logfile = '') {
 /**
  * Handle index view
  */
-$app->get('/', function () use ($app) {
-	$app->render('message.php', array(
-		'title' => 'Hei',
-		'message' => 'Käy <a href="http://vaasa.hacklab.fi/">Vaasa Hacklabin sivuilla</a>'
-	));
-})->name('index');
+$app->get(
+    '/',
+    function () use ($app) {
+        $app->render(
+            'message.php',
+            [
+                'title' => 'Hei',
+                'message' => 'Käy <a href="http://vaasa.hacklab.fi/">Vaasa Hacklabin sivuilla</a>'
+            ]
+        );
+    }
+)->name('index');
 
 /**
  * Handle logging phone number and optional message
  */
-$app->post('/log/', function () use ($app, $settings) {
-    $apiKey = $app->request->post('key');
-    $phoneNumber = $app->request->post('phone');
-    $message = $app->request->post('message');
+$app->post(
+    '/log/',
+    function () use ($app, $settings) {
+        $apiKey = $app->request->post('key');
+        $phoneNumber = $app->request->post('phone');
+        $message = $app->request->post('message');
 
-    // Check API key parameter
-    if (trim($apiKey) !== $settings['api_key']) {
-        $app->halt(403, 'API key access denied');
-    }
-
-    // Clean up POST data
-    $phoneNumber = preg_replace('/[^\d\w\b -.,:;]/', '', $phoneNumber);
-    $message = preg_replace('/[^\d\w\b -.,:;]/', '', $message);
-
-    try {
-        logiin($phoneNumber, $message, $settings['log_file']);
-    } catch (Exception $e) {
-        echo "FAIL: " . $e->getMessage();
-        return;
-    }
-
-    echo 'OK';
-});
-
-$app->get('/newest', function () use ($app, $settings) {
-    // Get last 1000 characters from log file, usually enough
-    $log = substr(
-        file_get_contents($settings['log_file']),
-        -1000
-    );
-
-    // Turn log content into array and reverse it, now newest line is first
-    $log_array = explode("\n", $log);
-    $log_array = array_reverse($log_array);
-
-    $result = array();
-
-    foreach($log_array as $row) {
-        // Skip empty rows from log array
-        if(empty($row)) {
-            continue;
+        // Check API key parameter
+        if (trim($apiKey) !== $settings['api_key']) {
+            $app->halt(403, 'API key access denied');
         }
 
-        // Get info from row
-        $info = explode(', ', $row);
-        $timestamp = strtotime($info[0]);
-        $username = trim(utf8_decode($info[2]));
+        // Clean up POST data
+        $phoneNumber = preg_replace('/[^\d\w\b -.,:;]/', '', $phoneNumber);
+        $message = preg_replace('/[^\d\w\b -.,:;]/', '', $message);
 
-        if($username === 'boot' || strtolower($username) === 'denied') {
-            continue;
+        try {
+            logiin($phoneNumber, $message, $settings['log_file']);
+        } catch (Exception $e) {
+            echo "FAIL: " . $e->getMessage();
+            return;
         }
 
-	break;
+        echo 'OK';
     }
+);
 
-    $result = 'Door last opened by \'' . $username . '\' ' . FuzzyTime::getFuzzyTime($timestamp);
+$app->get(
+    '/newest',
+    function () use ($app, $settings) {
+        // Get last 1000 characters from log file, usually enough
+        $log = substr(
+            file_get_contents($settings['log_file']),
+            -1000
+        );
 
-    echo $result;
-});
+        // Turn log content into array and reverse it, now newest line is first
+        $log_array = explode("\n", $log);
+        $log_array = array_reverse($log_array);
+
+        $result = [];
+
+        foreach($log_array as $row) {
+            // Skip empty rows from log array
+            if(empty($row)) {
+                continue;
+            }
+
+            // Get info from row
+            $info = explode(', ', $row);
+            $timestamp = strtotime($info[0]);
+            $username = trim(utf8_decode($info[2]));
+
+            if($username === 'boot' || strtolower($username) === 'denied') {
+                continue;
+            }
+
+        break;
+        }
+
+        $result = 'Door last opened by \'' . $username . '\' ' . FuzzyTime::getFuzzyTime($timestamp);
+
+        echo $result;
+    }
+);
 
 $app->run();
--- templates/message.php
+++ templates/message.php
@@ -1,9 +1,6 @@
 <?php include 'header.php'; ?>
-		
 		<h1><?php echo $title; ?></h1>
 		<p>
 			<?php echo $message; ?>
 		</p>
-
 <?php include 'footer.php'; ?>
-						
